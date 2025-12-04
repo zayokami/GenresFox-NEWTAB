8596d221af1c692c68c37c190dc6d6df3cbabce6
Zayoka <141497128+zayokami@users.noreply.github.com>
2025-12-04 18:53:49 +0800

    Added: some easter eggs.

--- src/script.js
+++ src/script.js
@@ -779,3 +779,8 @@ window.initRippleEffects = initRippleEffects;
 
 // Then run full init
 init();
+
+/** "And if I only could
+I'd make a deal with God
+And I'd get Him to swap our places" 
+- Kate Bush, "Running Up That Hill" (1985) */
